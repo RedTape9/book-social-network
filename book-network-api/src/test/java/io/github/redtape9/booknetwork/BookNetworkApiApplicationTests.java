cf799d6da899ab9481cc1e53041ecfa7ec846a3f
Sergej Jaudszims <redtape9688@gmail.com>
2024-05-07 12:28:23 +0200

    Initial commit

---
+++ book-network-api/src/test/java/io/github/redtape9/booknetwork/BookNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.redtape9.booknetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
