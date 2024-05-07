3d679e9e0639ad4f2d7439ef689f5cea9115046d
Sergej Jaudszims <redtape9688@gmail.com>
2024-05-07 15:56:29 +0200

    add Token and repos

---
+++ book-network-api/src/main/java/io/github/redtape9/booknetwork/user/Token.java
@@ -0,0 +1,30 @@
+package io.github.redtape9.booknetwork.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue
+    private Integer Id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "usewrId", nullable = false)
+    private User user;
+
+
+
+
+}
