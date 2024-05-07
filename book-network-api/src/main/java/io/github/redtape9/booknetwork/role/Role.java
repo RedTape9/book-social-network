7f0e751326a42edc64bbe1a9e23b22a8c41061a0
Sergej Jaudszims <redtape9688@gmail.com>
2024-05-07 15:38:48 +0200

    add Role Entity and relationship to User

---
+++ book-network-api/src/main/java/io/github/redtape9/booknetwork/role/Role.java
@@ -0,0 +1,39 @@
+package io.github.redtape9.booknetwork.role;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.github.redtape9.booknetwork.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Integer Id;
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+}
